20ac2cbf24b402c9dd89a8e3be0bddb0fff99568
Prashanth Loganathan <4logan88@gmail.com>
2018-09-16 13:53:54 +0100

    hello world example

---
+++ presentation.py
@@ -0,0 +1,15 @@
+# Builtin functions such as print, len, str, list, set doesn't need to be imported
+
+
+a = 5
+b = 10
+print(a + b)
+print(type(a))
+
+# Python variables are untyped until assigned
+a = 'Hello'
+b = 'World'
+print(a+b)
+print(a, b)
+print(type(a))
+
