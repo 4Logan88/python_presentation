372c8010487ebb66e73bd43ba74368134e252f07
Prashanth Loganathan <4logan88@gmail.com>
2018-09-17 21:28:31 +0100

    updated with simple collections

--- presentation.py
+++ presentation.py
@@ -1,6 +1,5 @@
 # Builtin functions such as print, len, str, list, set doesn't need to be imported
 
-
 a = 5
 b = 10
 print(a + b)
@@ -12,4 +11,26 @@ b = 'World'
 print(a+b)
 print(a, b)
 print(type(a))
+
+# Python list
+cars = ['audi', 'bmw', ['honda','merc'], 'vw']
+print('first element of cars is: {}'.format(cars[0]))
+print('last element of cars is: {}'.format(cars[-1]))
+print('cars[2][1]: {}'.format(cars[2][1]))
+cars[3] = 'toyota'
+print('full list of cars: {}'.format(cars))
+cars.append('vw')
+print('append vw to cars: {}'.format(cars))
+
+# tuples are immutable
+cars = ('audi', 'bmw', ('honda','merc'), 'vw')
+# cars[3] = 'toyota'
+
+# dictionaries
+currencies_dict = {'uk':'sterling', 'us':'dollar'}
+currencies_dict['germany'] = 'euro'
+print('currencies dictionary = {}'.format(currencies_dict))
+languages_dict = dict(uk='english', france='french')
+print('language dictionary = {}'.format(languages_dict))
+
 
